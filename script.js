ff03c30f29ba491f041f9794c90f10a452069b5a
zz-hh-aa <zzhhaa@protonmail.com>
2022-07-07 14:53:50 +0100

    start-results-return

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const comment = document.getElementById("comment");
-const submit = document.getElementById("submit");
+var name = document.getElementById("name");
+var email = document.getElementById("email");
+var comment = document.getElementById("comment");
+var submit = document.getElementById("submit");
 // needs to tell it what to do when clicked: const clicked = document.addEventListener("click",);
 // might need to remove?: const body = document.addEventListener("body");
-const nameResult = document.getElementById("name-result");
-const emailResult = document.getElementById("email-result");
-const commentResult = document.getElementById("comment-result");
 
 // CHARACTER COUNT
 const maxChars = 140;
@@ -14,54 +11,36 @@ const commentCounter = document.getElementById("comment-counter");
 comment.addEventListener('input', () => {
   const charCount = comment.value.length;
   commentCounter.textContent = `${charCount} /140`;
-  console.log(charCount)
+
+  const colour = charCount < 140 ? 'black' : 'white';
+  const weight = charCount < 140 ? 'normal' : 'bold';
+  commentCounter.style.color = colour;
+  commentCounter.style.fontWeight = weight;
 });
 
-// let commentChar = comment.split(""); 
-// let commentCount = {};
-// for (var i = 0; i < commentChars.length; i++) {
-//   if (commentCount[commentChars[i]] == undefined)
-//     commentCount[commentChars[i]] = 0;
-//   commentCount[commentChars[i]] += 1;
-// }
-// for (i in commentCount)
-// {
-//   console.log(i + ' ' + commentCount[i]);
-// }
-// let length = comment.str.length;
-// let commentCounter = document.getElementById("comment-counter")
-// commentCounter.append(length);
-
-// submitOK = "true";
-
-// if (Comment.length > 140) {
-//   alert("The name may have no more than 140 characters");
-//   submitOK = "false";
-// }
-// // else 
-// submitOK = "true";
-
 // RETURN RESULTS
+const nameResult = document.getElementById("name-result");
+const emailResult = document.getElementById("email-result");
+const commentResult = document.getElementById("comment-result");
 
 function showInput() {
-    newDiv.innerText = "name is: ", Name, "email is: ", Email, "comment is: ", Comment;
-    // submit.addEventListener('click',()=> {
-    newDiv.textContent = "name is: ", Name, "email is: ", Email, "comment is: ", Comment;
-    Body.append(newDiv)
+  nameResult.innerText = "name is: ", name.value;
+  emailResult.innerText = "email is: ", email.value;
+  commentResult.innerText = "comment is: ", comment.value;
+  console.log(name.value, email.value, comment.value);
+}
 
-    // if (submit === clicked) {
-    //     submittedName.innerHTML = name;
-    //     } else {
-    //     submittedName.innerHTML = "";
-        
-        // submittedName.innerHTML = "name is:", name;
-        // submittedEmail.innerHTML = "email is:", email;
-        // submittedComment.innerHTML = "comment is", comment;  
-    }
-    // )
+//   submit.addEventListener('click',()=> {
+//     if (submit === clicked) {
+//       nameResult.innerText = "name is: ", nameValue;
+//       emailResult.innerText = "email is: ", emailValue;
+//       commentResult.innerText = "comment is: ", commentValue;
+//       console.log(nameValue, emailValue, commentValue);
+//     } else {
+//       nameResult.innerText = "";
+//       emailResult.innerText = ""; 
+//       commentResult.innerText = "";
+//       console.log(null);
+//     }
+//   })
 // }
-
-// document.getElementById("results").innerHTML = 
-//                 document.getElementById("name").value;
-//                 document.getElementById("email").value;
-//                 document.getElementById("comment").value;
